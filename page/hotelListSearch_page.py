65ca32283f2eb19b157c8d7bc922bbdc70de1913
zzf916 <735836911@qq.com>
2022-02-14 00:37:39 +0800

    yyds

--- page/hotelListSearch_page.py
+++ page/hotelListSearch_page.py
@@ -5,7 +5,13 @@
 # Desc : 酒店搜索
 from selenium.webdriver.common.by import By
 
+from comm.basepage import BasePage
 
-class HotelListSearchPage(object):
+
+class HotelListSearchPage(BasePage):
     search = (By.XPATH, '//*[@resource-id="ctrip.android.view:id/a" and @text="关键字/位置/品牌/酒店名"]')  # 搜索框
     hotelList = (By.XPATH, '//*[@resource-id="ctrip.android.view:id/a" and @class="android.view.ViewGroup"]')  # 酒店列表
+
+    def action(self, hotelname):
+        self.locator(self.search).send_keys(hotelname)
+        self.locator(self.hotelList).click()
